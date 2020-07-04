1ae80283bcf5fff6e30d32cf5e9868c04cec75a3
Noppamas045 <noppamas.sang@mail.kmutt.ac.th>
2020-07-04 13:55:10 +0700

    3. Complete

--- PEXAM/PEXAM.ino
+++ PEXAM/PEXAM.ino
@@ -1,3 +1,7 @@
+const int M1A = 7;
+const int M2A =6;
+const int ena = 9;
+
 void setup(){
     Serial.begin(9600);
     pinMode(8,OUTPUT);
@@ -6,10 +10,22 @@ void setup(){
     noTone(8);
 
     Serial.println("Hello MDT !!!"); //2
+
+    pinMode(M1A, OUTPUT);
+    pinMode(M2A, OUTPUT);
+    pinMode(ena, OUTPUT);
+
+    analogWrite (ena,100);
+    digitalWrite (M1A,1);
+    digitalWrite (M2A,0);
+    Serial.println("Turn CCW");
+    delay(2000);
+    digitalWrite (M1A,0);
+    digitalWrite (M2A,0);
+
+    
 }
 
-void loop(){
-  
-    
+void loop(){ 
 
 }
