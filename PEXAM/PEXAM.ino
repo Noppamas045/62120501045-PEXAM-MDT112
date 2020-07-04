ec49230ba15b37b7ed2f3e6cdece07fe096f3bb5
Noppamas045 <noppamas.sang@mail.kmutt.ac.th>
2020-07-04 14:42:52 +0700

    5. Complete

--- PEXAM/PEXAM.ino
+++ PEXAM/PEXAM.ino
@@ -1,6 +1,14 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
 const int M1A = 7;
 const int M2A =6;
 const int ena = 9;
+
+Adafruit_SSD1306 display = Adafruit_SSD1306(128,32,&Wire);
 
 void setup(){
     Serial.begin(9600);
@@ -31,9 +39,25 @@ void setup(){
     digitalWrite (M1A,0);
     digitalWrite (M2A,0); //4
 
+    
+    display.begin(0x3C);
+    display.clearDisplay();
+    display.display();
+    
+    display.setCursor(30,10);
+    display.setTextColor(1);
+    display.setTextSize(1);
+    display.println("Hello MDT !!");
+      display.display(); 
+      delay(1000);
+      display.clearDisplay();
+      display.display(); //5
+
+
 
 }
 
 void loop(){ 
+
 
 }
